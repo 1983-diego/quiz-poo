34a5745f890aee2e436ff3cd53c1ee202d460898
Diego Passos dos Santos <diegopassosdossantos@pop-os.localdomain>
2023-06-06 10:22:40 -0300

    app concluded

---
+++ data/data.js
@@ -0,0 +1,18 @@
+export const data = [
+    {
+        question: "What is not a code editor?",
+        choices: ["vim", "vscode", "emacs", "word"],
+        answer: "word"
+    },
+    {
+        question: "What is not a oriented object language?",
+        choices: ["java", "haskell", "c++", "python"],
+        answer: "haskell"
+    },
+    {
+        question: "What is not a style language for websites?",
+        choices: ["stylus", "less", "sass", "postcss"],
+        answer: "postcss"
+    },
+]
+
